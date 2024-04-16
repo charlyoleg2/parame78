829bfbe408d3429d1f80eb8c41fd6ffb284b28cf
charles <charles.braquet@e.email>
2024-04-17 00:22:46 +0200

    paxScr works also for sub-designs

--- desi78/src/index.ts
+++ desi78/src/index.ts
@@ -2,3 +2,4 @@
 
 export * from './facade/door';
 export * from './menuiserie/cabane_plancher';
+export * from './menuiserie/cabane';
---
+++ desi78/src/menuiserie/cabane.ts
@@ -0,0 +1,186 @@
+// cabane.ts
+// une cabane pour les enfants
+
+// step-1 : import from geometrix
+import type {
+	//tContour,
+	tParamDef,
+	tParamVal,
+	tGeom,
+	//tExtrude,
+	//tSubInst,
+	//tSubDesign,
+	tPageDef
+} from 'geometrix';
+import {
+	designParam,
+	checkGeom,
+	prefixLog,
+	//point,
+	//Point,
+	//ShapePoint,
+	//vector,
+	//contour,
+	//contourCircle,
+	//ctrRectangle,
+	figure,
+	//degToRad,
+	//radToDeg,
+	ffix,
+	pNumber,
+	//pCheckbox,
+	//pDropdown,
+	pSectionSeparator,
+	initGeom,
+	EExtrude,
+	EBVolume
+} from 'geometrix';
+
+// design import
+import { cabanePlancherDef } from './cabane_plancher';
+
+// step-2 : definition of the parameters and more (part-name, svg associated to each parameter, simulation parameters)
+const pDef: tParamDef = {
+	// partName is used in URL. Choose a name without slash, backslash and space.
+	partName: 'cabane',
+	params: [
+		//pNumber(name, unit, init, min, max, step)
+		pNumber('N1', 'planches', 8, 2, 100, 1),
+		pNumber('N2', 'planches', 13, 2, 100, 1),
+		pNumber('W1', 'mm', 200, 20, 800, 1),
+		pNumber('W2', 'mm', 200, 20, 800, 1),
+		pSectionSeparator('Pilotis face'),
+		pNumber('H1', 'mm', 400, 10, 2000, 10),
+		pNumber('H2', 'mm', 150, 10, 400, 1),
+		pNumber('H3', 'mm', 150, 10, 400, 1),
+		pNumber('W3', 'mm', 150, 10, 400, 1),
+		pNumber('a1', 'degree', 80, 20, 90, 1),
+		pNumber('W4', 'mm', 150, 10, 400, 1),
+		pNumber('W5', 'mm', 100, 10, 400, 1),
+		pNumber('E1', 'mm', 200, 0, 1000, 10),
+		pSectionSeparator('Pilotis side'),
+		pNumber('T1', 'mm', 20, 1, 300, 1),
+		pNumber('S1', 'mm', 200, 0, 1000, 10),
+		pNumber('S2', 'mm', 100, 10, 400, 10)
+	],
+	paramSvg: {
+		N1: 'cabane_plancher_top.svg',
+		N2: 'cabane_plancher_bottom.svg',
+		W1: 'cabane_plancher_top.svg',
+		W2: 'cabane_plancher_bottom.svg',
+		H1: 'cabane_plancher_face.svg',
+		H2: 'cabane_plancher_face.svg',
+		H3: 'cabane_plancher_face.svg',
+		W3: 'cabane_plancher_face.svg',
+		a1: 'cabane_plancher_face.svg',
+		W4: 'cabane_plancher_face.svg',
+		W5: 'cabane_plancher_face.svg',
+		E1: 'cabane_plancher_face.svg',
+		T1: 'cabane_plancher_face.svg',
+		S1: 'cabane_plancher_side.svg',
+		S2: 'cabane_plancher_side.svg'
+	},
+	sim: {
+		tMax: 100,
+		tStep: 0.5,
+		tUpdate: 500 // every 0.5 second
+	}
+};
+
+// step-3 : definition of the function that creates from the parameter-values the figures and construct the 3D
+function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
+	const rGeome = initGeom(pDef.partName + suffix);
+	const figPlancherTop = figure();
+	const figPlancherBottom = figure();
+	const figPlancherFace = figure();
+	const figPlancherSide = figure();
+	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
+	try {
+		// step-4 : some preparation calculation
+		const paramA = param.N1 * param.W1;
+		const paramB = param.N2 * param.W2;
+		const goldenRatio = 1.618;
+		const ratioBA = paramB / paramA;
+		// step-5 : checks on the parameter values
+		// step-6 : any logs
+		rGeome.logstr += `cabane-plancher-size: A: ${ffix(paramA)} m, B: ${ffix(paramB)} m, surface: ${ffix(paramA * paramB)} m2\n`;
+		rGeome.logstr += `Comparison with the golden-ratio: B/A: ${ffix(ratioBA)}   ${ffix((100 * ratioBA) / goldenRatio)} %\n`;
+		// step-7 : drawing of the figures
+		// sub-desingn
+		const plancherParam = designParam(cabanePlancherDef.pDef, '');
+		plancherParam.setVal('N1', param.N1);
+		plancherParam.setVal('W1', param.W1);
+		plancherParam.setVal('N2', param.N2);
+		plancherParam.setVal('W2', param.W2);
+		const plancherGeom = cabanePlancherDef.pGeom(
+			0,
+			plancherParam.getParamVal(),
+			plancherParam.getSuffix()
+		);
+		checkGeom(plancherGeom);
+		rGeome.logstr += prefixLog(plancherGeom.logstr, plancherParam.getPartNameSuffix());
+		// figPlancherTop
+		figPlancherTop.mergeFigure(plancherGeom.fig.facePlancherTop);
+		// figPlancherBottom
+		figPlancherBottom.mergeFigure(plancherGeom.fig.facePlancherBottom);
+		// figPlancherFace
+		// figPlancherSide
+		// final figure list
+		rGeome.fig = {
+			facePlancherTop: figPlancherTop,
+			facePlancherBottom: figPlancherBottom,
+			facePlancherFace: figPlancherFace,
+			facePlancherSide: figPlancherSide
+		};
+		// step-8 : recipes of the 3D construction
+		const designName = rGeome.partName;
+		rGeome.vol = {
+			extrudes: [
+				{
+					outName: `subpax_${designName}`,
+					face: `${designName}_facePlancherTop`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: param.T1,
+					rotate: [0, 0, 0],
+					translate: [0, 0, 0]
+				}
+			],
+			volumes: [
+				{
+					outName: `pax_${designName}`,
+					boolMethod: EBVolume.eIdentity,
+					inList: [`subpax_${designName}`]
+				}
+			]
+		};
+		// step-9 : optional sub-design parameter export
+		// sub-design
+		rGeome.sub = {
+			thePlancher: {
+				partName: plancherParam.getPartName(),
+				dparam: plancherParam.getDesignParamList(),
+				orientation: [0, 0, 0],
+				position: [0, 0, 567]
+			}
+		};
+		// step-10 : final log message
+		// finalize
+		rGeome.logstr += 'cabane drawn successfully!\n';
+		rGeome.calcErr = false;
+	} catch (emsg) {
+		rGeome.logstr += emsg as string;
+		console.log(emsg as string);
+	}
+	return rGeome;
+}
+
+// step-11 : definiton of the final object that gathers the precedent object and function
+const cabaneDef: tPageDef = {
+	pTitle: 'Cabane',
+	pDescription: 'La cabane des enfants',
+	pDef: pDef,
+	pGeom: pGeom
+};
+
+// step-12 : export the final object
+export { cabaneDef };
