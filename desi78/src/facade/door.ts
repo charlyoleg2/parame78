7f8ff0e5675626515832f31a9228d5fb7a5c8c16
charles <charles.braquet@e.email>
2024-04-16 22:37:49 +0200

    add comment for partName

--- desi78/src/facade/door.ts
+++ desi78/src/facade/door.ts
@@ -35,6 +35,7 @@ import {
 
 // step-2 : definition of the parameters and more (part-name, svg associated to each parameter, simulation parameters)
 const pDef: tParamDef = {
+	// partName is used in URL. Choose a name without slash, backslash and space.
 	partName: 'door',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
