cf060b5dfd337e51c10d2de00c1b19fb4713d35c
charlyoleg <charles.braquet@gmx.fr>
2024-05-03 20:34:32 +0200

    wave def

--- desi78/src/chaudronnerie/reinforced_tube.ts
+++ desi78/src/chaudronnerie/reinforced_tube.ts
@@ -25,7 +25,7 @@ import {
 	ctrRectangle,
 	figure,
 	//degToRad,
-	//radToDeg,
+	radToDeg,
 	ffix,
 	pNumber,
 	//pCheckbox,
@@ -50,7 +50,7 @@ const pDef: tParamDef = {
 		pNumber('N2', 'wave', 20, 4, 400, 1),
 		pNumber('W22', 'mm', 80, 1, 600, 1),
 		pDropdown('wave_method', ['S23L_R32', 'D3_R32']),
-		pNumber('S23L', 'mm', 0, 0, 200, 1),
+		pNumber('S23L', 'mm', 200, 1, 600, 1),
 		pNumber('R32', '%', 50, 10, 90, 0.1),
 		pNumber('D2', 'mm', 20, 1, 600, 1),
 		pNumber('D3', 'mm', 20, 1, 600, 1),
@@ -87,9 +87,39 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	try {
 		// step-4 : some preparation calculation
 		const R1Li = param.D1L / 2 - param.E1;
+		const aW22 = 2 * Math.asin(param.W22 / (2 * R1Li));
+		const LextW22 = (aW22 * param.D1L) / 2;
+		const LintW22 = aW22 * R1Li;
+		const aN2 = (2 * Math.PI) / param.N2;
+		const aWave = aN2 - 2 * aW22;
+		const D2e = R1Li * aWave * 0.5; // approximate arbitrary value
+		const R2e = D2e / 2;
+		const R2i = R2e - param.E2;
+		const R3i = (R2i * (1 - param.R32)) / param.R32;
+		const R3e = R3i + param.E2;
+		const WaveCordeInt = aWave * R1Li;
 		// step-5 : checks on the parameter values
+		if (param.S23L < param.E2) {
+			throw `err095: S23L ${param.S23L} is too small compare to E2 ${param.E2}`;
+		}
+		if (R1Li - param.S23L < 0) {
+			throw `err098: D1L ${param.D1L} is too small compare to E1 ${param.E1} or E2 ${param.E2}`;
+		}
+		if (aWave < 0) {
+			throw `err105: N2 ${param.N2} is too large compare to D1L ${param.D1L} and E1 ${param.E1}`;
+		}
+		if (WaveCordeInt < 2 * (R2e + R3e)) {
+			if (param.S23L < 2 * (R2e + R3e)) {
+				throw `err113: S23L ${param.S23L} is too small compare to D1L ${param.D1L} and N2 ${param.N2}`;
+			}
+		}
 		// step-6 : any logs
-		rGeome.logstr += `reinforced-tube internal-external diameter: D1Li: ${ffix(2 * R1Li)} mm\n`;
+		rGeome.logstr += `reinforced_tube internal-external diameter: D1Li: ${ffix(2 * R1Li)} mm\n`;
+		rGeome.logstr += `W22: angle: ${ffix(radToDeg(aW22))} degree, corde-ext: ${ffix(LextW22)} mm, corde-int: ${ffix(LintW22)} mm\n`;
+		rGeome.logstr += `W2: angle: ${ffix(radToDeg(2 * aW22))} degree, corde-ext: ${ffix(2 * LextW22)} mm, corde-int: ${ffix(2 * LintW22)} mm\n`;
+		rGeome.logstr += `Wave: angle: ${ffix(radToDeg(aWave))} degree, corde-int: ${ffix(WaveCordeInt)} mm\n`;
+		rGeome.logstr += `D2: ${ffix(2 * R2i)} mm, R2i: ${ffix(R2i)} mm, R2e: ${ffix(R2e)} mm\n`;
+		rGeome.logstr += `D3: ${ffix(2 * R3i)} mm, R3i: ${ffix(R3i)} mm, R3e: ${ffix(R3e)} mm\n`;
 		// step-7 : drawing of the figures
 		// figTopExt
 		figTopExt.addMain(contourCircle(0, 0, param.D1L / 2));
@@ -131,7 +161,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		// sub-design
 		// step-10 : final log message
 		// finalize
-		rGeome.logstr += 'reinforced-tube drawn successfully!\n';
+		rGeome.logstr += 'reinforced_tube drawn successfully!\n';
 		rGeome.calcErr = false;
 	} catch (emsg) {
 		rGeome.logstr += emsg as string;
