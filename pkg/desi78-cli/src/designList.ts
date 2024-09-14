d0bfb47b4c213b6d31d619db2be5ea7f8ad7a8d6
charlyoleg <charles.braquet@gmx.fr>
2024-09-14 22:41:01 +0200

    fix design name

--- pkg/desi78-cli/src/designList.ts
+++ pkg/desi78-cli/src/designList.ts
@@ -5,7 +5,7 @@ import { doorDef, cabane_plancherDef, cabaneDef, reinforced_tubeDef, reinforced_
 
 const designList: tAllPageDef = {
 	'desi78/facade/door': doorDef,
-	'desi78/menuiserie/cabane_plancher': cabane_plancherDef,
+	'desi78/menuiserie/cabane_plancher': cabanePlancherDef,
 	'desi78/menuiserie/cabane': cabaneDef,
 	'desi78/chaudronnerie/reinforced_tube': reinforced_tubeDef,
 	'desi78/chaudronnerie/reinforced_cone': reinforced_coneDef
