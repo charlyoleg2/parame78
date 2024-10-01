0a071685f22f38f7c71ce0f365cd7745fc0e7ce7
charlyoleg <charles.braquet@gmx.fr>
2024-10-01 21:47:37 +0200

    rail completed

--- pkg/desi78/src/train/rail.ts
+++ pkg/desi78/src/train/rail.ts
@@ -3,7 +3,7 @@
 
 // step-1 : import from geometrix
 import type {
-	Contour,
+	//Contour,
 	//tContour,
 	//tOuterInner,
 	tParamDef,
@@ -42,17 +42,17 @@ const pDef: tParamDef = {
 	params: [
 		//pNumber(name, unit, init, min, max, step)
 		pNumber('W1', 'mm', 200, 2, 400, 1),
-		pNumber('W2', 'mm', 4, 0.5, 40, 0.5),
+		pNumber('W2', 'mm', 10, 0.5, 40, 0.5),
 		pNumber('W3', 'mm', 100, 1, 200, 1),
 		pSectionSeparator('Heights'),
-		pNumber('H1', 'mm', 3, 1, 30, 1),
+		pNumber('H1', 'mm', 6, 1, 30, 1),
 		pNumber('H2', 'mm', 10, 1, 50, 1),
 		pNumber('H3', 'mm', 100, 1, 500, 1),
 		pNumber('H4', 'mm', 10, 1, 50, 1),
 		pNumber('H5', 'mm', 30, 1, 100, 1),
 		pNumber('H6', 'mm', 10, 1, 100, 1),
 		pSectionSeparator('Corners'),
-		pNumber('R1', 'mm', 3, 1, 30, 1),
+		pNumber('R1', 'mm', 3, 0, 30, 1),
 		pSectionSeparator('Depth'),
 		pNumber('L1', 'mm', 100, 1, 1000, 1)
 	],
@@ -76,89 +76,59 @@ const pDef: tParamDef = {
 	}
 };
 
-// sub-functions
-function e1plus(aRcurve: number, aDiameter: number, aType: number): number {
-	let rPlus = 0; // planar case
-	const delta = aDiameter * (1 - Math.sqrt(1 - (aDiameter / (2 * aRcurve)) ** 2));
-	if (aType === 0) {
-		// convex case
-		rPlus = delta;
-	} else if (aType === 2) {
-		// concave case
-		rPlus = -1 * delta;
-	}
-	return rPlus;
-}
-
 // step-3 : definition of the function that creates from the parameter-values the figures and construct the 3D
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
-	const figLensSim = figure();
-	const figLens3D = figure();
+	const figRail = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
-		const Dr2 = param.Dr / 2;
-		const Dl2 = param.Dl / 2;
-		const E1pL = e1plus(param.Rl, param.Dl, param.TypeL);
-		const E1pR = e1plus(param.Rr, param.Dr, param.TypeR);
-		const E2 = param.E1 - E1pL - E1pR;
-		const E1h = param.E1 / 2;
-		const D1h = param.D1 / 2;
+		const W1h = param.W1 / 2;
+		const W2h = param.W2 / 2;
+		const W3h = param.W3 / 2;
+		const H16 = param.H1 + param.H2 + param.H3 + param.H4 + param.H5 + param.H6;
 		// step-5 : checks on the parameter values
-		if (param.Dr > param.D1) {
-			throw `err390: Dr ${param.Dr} is larger than D1 ${param.D1}`;
+		if (param.W2 >= param.W1) {
+			throw `err210: W2 ${param.W2} is larger than W1 ${param.W1}`;
 		}
-		if (param.Dl > param.D1) {
-			throw `err393: Dl ${param.Dl} is larger than D1 ${param.D1}`;
-		}
-		if (E2 < 0) {
-			throw `err396: E2 ${ffix(E2)} is negative because of E1pL ${ffix(E1pL)} or E1pR ${ffix(E1pR)}`;
+		if (param.W2 >= param.W3) {
+			throw `err213: W2 ${param.W2} is larger than W3 ${param.W3}`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `E2: ${ffix(E2)} mm\n`;
+		rGeome.logstr += `total height: ${ffix(H16)} mm\n`;
 		// step-7 : drawing of the figures
-		// figLensSim
-		function ctrHalfLens(aYS: number, aClose: boolean): Contour {
-			const rCtr = contour(E1h, 0);
-			if (param.TypeR !== 1) {
-				rCtr.addPointA(E1h - E1pR, aYS * Dr2).addSegArc3((aYS * Math.PI) / 2, true);
-			}
-			rCtr.addSegStrokeA(E1h - E1pR, aYS * D1h).addSegStrokeA(-E1h + E1pL, aYS * D1h);
-			if (param.TypeL !== 1) {
-				rCtr.addSegStrokeA(-E1h + E1pL, aYS * Dl2)
-					.addPointA(-E1h, 0)
-					.addSegArc3((aYS * Math.PI) / 2, false);
-			} else {
-				rCtr.addSegStrokeA(-E1h, 0);
-			}
-			if (aClose) {
-				rCtr.closeSegStroke();
-			}
-			return rCtr;
-		}
-		const ctrLens = ctrHalfLens(1, false);
-		// second half of the lens
-		if (param.TypeL !== 1) {
-			ctrLens.addPointA(-E1h + E1pL, -Dl2).addSegArc3(-Math.PI / 2, true);
-		}
-		ctrLens.addSegStrokeA(-E1h + E1pL, -D1h).addSegStrokeA(E1h - E1pR, -D1h);
-		if (param.TypeR !== 1) {
-			ctrLens
-				.addSegStrokeA(E1h - E1pR, -Dr2)
-				.addPointA(E1h, 0)
-				.addSegArc3(-Math.PI / 2, false);
-		} else {
-			ctrLens.addSegStrokeA(E1h, 0);
-		}
-		figLensSim.addMainO(ctrLens);
-		// figLens3D
-		figLens3D.addMainO(ctrHalfLens(1, true).rotate(0, 0, Math.PI / 2));
-		figLens3D.addSecond(ctrHalfLens(-1, true).rotate(0, 0, Math.PI / 2));
+		// figRail
+		const ctrRail = contour(W1h, 0)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, param.H1)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(W2h - W1h, param.H2)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, param.H3)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(W3h - W2h, param.H4)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, param.H5)
+			.addCornerRounded(param.R1)
+			.addPointA(0, H16)
+			.addPointA(-W3h, H16 - param.H6)
+			.addSegArc2()
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, -param.H5)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(W3h - W2h, -param.H4)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, -param.H3)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(W2h - W1h, -param.H2)
+			.addCornerRounded(param.R1)
+			.addSegStrokeR(0, -param.H1)
+			.addCornerRounded(param.R1)
+			.closeSegStroke();
+		figRail.addMainO(ctrRail);
 		// final figure list
 		rGeome.fig = {
-			faceLensSim: figLensSim,
-			faceLens3D: figLens3D
+			faceRail: figRail
 		};
 		// step-8 : recipes of the 3D construction
 		const designName = rGeome.partName;
@@ -166,8 +136,9 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			extrudes: [
 				{
 					outName: `subpax_${designName}`,
-					face: `${designName}_faceLens3D`,
-					extrudeMethod: EExtrude.eRotate,
+					face: `${designName}_faceRail`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: param.L1,
 					rotate: [0, 0, 0],
 					translate: [0, 0, 0]
 				}
