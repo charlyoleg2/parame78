e8df3f4ab519ea747657eb5bea8e4a1ad897c82f
charlyoleg <charles.braquet@gmx.fr>
2024-10-05 06:46:47 +0200

    ray intersection

--- pkg/desi78/src/optics/optic_sim.ts
+++ pkg/desi78/src/optics/optic_sim.ts
@@ -35,14 +35,6 @@ interface tLens {
 	PosX: number;
 }
 
-interface tRayInLens {
-	x1: number;
-	y1: number;
-	x2: number;
-	y2: number;
-	a2: number;
-}
-
 interface tDioptre {
 	n0: number;
 	n1: number;
@@ -65,15 +57,30 @@ interface tInterX {
 }
 
 const c_simOne = 0;
-//const c_simTwo = 1;
+const c_simTwo = 1;
 //const c_simParallel = 2;
 //const c_simObject = 3;
 
+// line intersection
+function lineXline(rs1: tRaySeg, rs2: tRaySeg): tInterX {
+	// ta1 = tan(a1); ta2 = tan(a2)
+	// y = ta1 * (x - x1) + y1 = ta2 * (x - x2) + y2
+	// x * (ta1 - ta2) = y2 - y1 - x2 * ta2 + x1 * ta1
+	const ta1 = Math.tan(rs1.aa);
+	const ta2 = Math.tan(rs2.aa);
+	const ta12 = ta1 - ta2;
+	if (ta12 === 0) {
+		throw `err073: error of parallel line intersection. a1: ${ffix(rs1.aa)} a2: ${ffix(rs2.aa)}`;
+	}
+	const ix = (rs2.yy - rs1.yy - rs2.xx * ta2 + rs1.xx * ta1) / ta12;
+	const iy = ta1 * (ix - rs1.xx) + rs1.yy;
+	const rInterX: tInterX = { ix: ix, iy: iy, refR: 0 };
+	return rInterX;
+}
+
 // quadratic equation
 function lineXcircle(
-	x0: number,
-	y0: number,
-	a0: number,
+	rs: tRaySeg,
 	cx: number,
 	radius: number,
 	signe: number,
@@ -90,16 +97,16 @@ function lineXcircle(
 	// B = 2*ta*(y1-x1*ta)-2*cx
 	// C = (y1-x1*ta)**2-cr**2+cx**2
 	// D = B**2-4*A*C
-	const ta = Math.tan(a0);
+	const ta = Math.tan(rs.aa);
 	const AA = ta ** 2 + 1;
-	const BB = 2 * ta * (y0 - x0 * ta) - 2 * cx;
-	const CC = (y0 - x0 * ta) ** 2 - radius ** 2 + cx ** 2;
+	const BB = 2 * ta * (rs.yy - rs.xx * ta) - 2 * cx;
+	const CC = (rs.yy - rs.xx * ta) ** 2 - radius ** 2 + cx ** 2;
 	const DD = BB ** 2 - 4 * AA * CC;
 	if (DD < 0) {
 		throw `err090: D ${ffix(DD)} < 0. The ray is out of the circle`;
 	}
 	const ix = (-BB - signe * Math.sqrt(DD)) / (2 * AA); // select opening or closing with signe
-	const iy = y0 + (ix - x0) * ta;
+	const iy = rs.yy + (ix - rs.xx) * ta;
 	const refR = withinHPiHPi(Math.atan2(iy, ix - cx));
 	if (Math.abs(refR) > angle) {
 		throw `err097: refR ${ffix(refR)} out of lens-angle ${ffix(angle)}`;
@@ -109,19 +116,19 @@ function lineXcircle(
 }
 
 // a single ray, one dioptre
-function traceDioptre(x0: number, y0: number, a0: number, diop: tDioptre): tRaySeg {
+function traceDioptre(rs: tRaySeg, diop: tDioptre): tRaySeg {
 	const rRaySeg: tRaySeg = { xx: 0, yy: 0, aa: 0 };
 	if (diop.signe === 0) {
 		// stroke
 		rRaySeg.xx = diop.cx;
-		rRaySeg.yy = y0 + (diop.cx - x0) * Math.tan(a0);
-		rRaySeg.aa = Math.asin((Math.sin(a0) * diop.n0) / diop.n1);
+		rRaySeg.yy = rs.yy + (diop.cx - rs.xx) * Math.tan(rs.aa);
+		rRaySeg.aa = Math.asin((Math.sin(rs.aa) * diop.n0) / diop.n1);
 	} else {
 		// opening circle and closing circle
-		const interX = lineXcircle(x0, y0, a0, diop.cx, diop.radius, diop.signe, diop.angle);
+		const interX = lineXcircle(rs, diop.cx, diop.radius, diop.signe, diop.angle);
 		rRaySeg.xx = interX.ix;
 		rRaySeg.yy = interX.iy;
-		rRaySeg.aa = interX.refR + Math.asin((Math.sin(a0 - interX.refR) * diop.n0) / diop.n1);
+		rRaySeg.aa = interX.refR + Math.asin((Math.sin(rs.aa - interX.refR) * diop.n0) / diop.n1);
 	}
 	return rRaySeg;
 }
@@ -138,13 +145,7 @@ function typeToSign(typ: number): number {
 }
 
 // a single ray, one lens
-function traceOneRayInLens(
-	x0: number,
-	y0: number,
-	a0: number,
-	n0: number,
-	lens: tLens
-): tRayInLens {
+function traceOneRayInLens(rs0: tRaySeg, n0: number, lens: tLens): [tRaySeg, tRaySeg] {
 	const dioptre1: tDioptre = {
 		n0: n0,
 		n1: lens.ni,
@@ -161,17 +162,16 @@ function traceOneRayInLens(
 		radius: lens.Rr,
 		angle: Math.asin(lens.Dr / (2 * lens.Rr))
 	};
-	const rs1 = traceDioptre(x0, y0, a0, dioptre1);
-	const rs2 = traceDioptre(rs1.xx, rs1.yy, rs1.aa, dioptre2);
-	const rRayInLens: tRayInLens = { x1: rs1.xx, y1: rs1.yy, x2: rs2.xx, y2: rs2.yy, a2: rs2.aa };
-	return rRayInLens;
+	const rs1 = traceDioptre(rs0, dioptre1);
+	const rs2 = traceDioptre(rs1, dioptre2);
+	return [rs1, rs2];
 }
 
 // intersection between a line (lx, ly, la) and a wall (a vertical line at wx)
-function lineXwall(lx: number, ly: number, la: number, wx: number): number {
-	const dx = wx - lx;
-	const dy = dx * Math.tan(la);
-	const rwy = ly + dy;
+function lineXwall(rs: tRaySeg, wx: number): number {
+	const dx = wx - rs.xx;
+	const dy = dx * Math.tan(rs.aa);
+	const rwy = rs.yy + dy;
 	return rwy;
 }
 
@@ -183,22 +183,20 @@ function traceOneRay(
 	n0: number,
 	lenss: tLens[],
 	imgPx: number
-): tContour {
+): [tContour, tRaySeg] {
 	const rCtrRay1 = contour(objPx, objPy, 'yellow');
-	let x0 = objPx;
-	let y0 = objPy;
-	let a0 = rayAngle1;
+	const rs0: tRaySeg = { xx: objPx, yy: objPy, aa: rayAngle1 };
 	for (const lens of lenss) {
-		const ris = traceOneRayInLens(x0, y0, a0, n0, lens);
-		rCtrRay1.addSegStrokeA(ris.x1, ris.y1).addSegStrokeA(ris.x2, ris.y2);
-		x0 = ris.x2;
-		y0 = ris.y2;
-		a0 = ris.a2;
+		const [rs1, rs2] = traceOneRayInLens(rs0, n0, lens);
+		rCtrRay1.addSegStrokeA(rs1.xx, rs1.yy).addSegStrokeA(rs2.xx, rs2.yy);
+		rs0.xx = rs2.xx;
+		rs0.yy = rs2.yy;
+		rs0.aa = rs2.aa;
 	}
 	const wallX = imgPx * 1.2;
-	const wallY = lineXwall(x0, y0, a0, wallX);
+	const wallY = lineXwall(rs0, wallX);
 	rCtrRay1.addSegStrokeA(wallX, wallY);
-	return rCtrRay1;
+	return [rCtrRay1, rs0];
 }
 
 // entry function for all simulation types
@@ -214,14 +212,10 @@ function rayTrace(
 	imgPx: number
 ): [tContour[], string] {
 	const rays: tContour[] = [];
-	let logSim = 'Optic simulator:\n';
 	const ray1A1 = degToRad(ray1Angle);
 	const ray2A1 = degToRad(ray2Angle);
-	if (simType === c_simOne) {
-		const ctrRay1 = traceOneRay(objPx, objPy, ray1A1, n0, lenss, imgPx);
-		rays.push(ctrRay1);
-	}
-	logSim += `ray1-angle1: ${ffix(radToDeg(ray1A1))}
+	let logSim = `Optic simulator:
+ray1-angle1: ${ffix(radToDeg(ray1A1))}
 ray2-angle1: ${ffix(radToDeg(ray2A1))}
 rayNb: ${rayNb}
 simType: ${simType}\n`;
@@ -233,8 +227,18 @@ simType: ${simType}\n`;
   index of refraction of the lens: ni: ${lenss[idx].ni}
   Position-X: ${lenss[idx].PosX} mm\n`;
 	}
-	logSim += `index of refraction of the environment: n0: ${n0}
-End of simulator\n`;
+	logSim += `index of refraction of the environment: n0: ${n0}\n`;
+	if (simType === c_simOne) {
+		const [ctrRay1] = traceOneRay(objPx, objPy, ray1A1, n0, lenss, imgPx);
+		rays.push(ctrRay1);
+	} else if (simType === c_simTwo) {
+		const [ctrRay1, rs1] = traceOneRay(objPx, objPy, ray1A1, n0, lenss, imgPx);
+		const [ctrRay2, rs2] = traceOneRay(objPx, objPy, ray2A1, n0, lenss, imgPx);
+		rays.push(ctrRay1, ctrRay2);
+		const pi = lineXline(rs1, rs2);
+		logSim += `ray intersection:  ix: ${ffix(pi.ix)}, iy: ${ffix(pi.iy)}\n`;
+	}
+	logSim += `End of simulator\n`;
 	return [rays, logSim];
 }
 
