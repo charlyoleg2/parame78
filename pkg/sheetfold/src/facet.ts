5afe1b2846252a8468c5bb2af2e7955ed821c2e3
charlyoleg <charles.braquet@gmx.fr>
2025-02-20 20:25:34 +0100

    reorganise sheetfold

---
+++ pkg/sheetfold/src/facet.ts
@@ -0,0 +1,80 @@
+// facet.ts
+
+import type { tContour } from 'geometrix';
+import { Contour } from 'geometrix';
+
+enum tJDir {
+	eA,
+	eB
+}
+
+enum tJSide {
+	eABRight,
+	eABLeft
+}
+
+/**
+ * class `ContourJ`
+ *
+ */
+class ContourJ extends Contour {
+	/** @internal */
+	junctionID: string[] = [];
+	junctionDir: tJDir[] = [];
+	junctionSide: tJSide[] = [];
+	junctionPosition: number[] = [];
+	lastPosition = -1;
+	startJunction(jName: string, aNb: tJDir, abSide: tJSide): ContourJ {
+		//console.log(`dbg822: jName: ${jName} aNb: ${aNb}`);
+		if (this.junctionID.includes(jName)) {
+			throw `err209: junctionID ${jName} already used`;
+		}
+		const newPosition = this.segments.length - 1;
+		if (newPosition === this.lastPosition) {
+			throw `err210: junctionID ${jName} overwrites previous junction`;
+		}
+		this.lastPosition = newPosition;
+		this.junctionID.push(jName);
+		this.junctionDir.push(aNb);
+		this.junctionSide.push(abSide);
+		this.junctionPosition.push(newPosition);
+		return this;
+	}
+}
+
+type tContourJ = tContour | ContourJ;
+
+/**
+ * class `Facet`
+ *
+ */
+class Facet {
+	/** @internal */
+	attached = false;
+	ax = 0;
+	ay = 0;
+	aa = 0;
+	juncIdx = 0;
+	outerInner: tContourJ[] = [];
+	constructor(iOuterInner: tContourJ[]) {
+		for (const iCtr of iOuterInner) {
+			this.outerInner.push(iCtr);
+		}
+	}
+}
+
+// instantiation functions
+function contourJ(ix: number, iy: number, icolor = ''): ContourJ {
+	return new ContourJ(ix, iy, icolor);
+}
+function facet(iOuterInner: tContourJ[]): Facet {
+	return new Facet(iOuterInner);
+}
+
+// other helper functions
+function contourJ2contour(iContoutJ: tContourJ): tContour {
+	return iContoutJ;
+}
+
+export type { tContourJ };
+export { tJDir, tJSide, ContourJ, Facet, contourJ, facet, contourJ2contour };
